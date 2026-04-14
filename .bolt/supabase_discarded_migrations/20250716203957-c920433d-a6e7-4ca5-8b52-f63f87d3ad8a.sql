0119603571c7f6fa73b6693da0c2eedfeba01dd3
TeeMhor275 <ajibodegbolahan275@gmail.com>
2026-04-14 14:45:14 -0700

    chore: remove discarded Supabase migrations and development configuration files

--- .bolt/supabase_discarded_migrations/20250716203957-c920433d-a6e7-4ca5-8b52-f63f87d3ad8a.sql
+++
@@ -1,5 +0,0 @@
--- Allow anyone to insert access codes (needed for signup flow)
-CREATE POLICY "Anyone can request access codes" 
-ON public.access_codes 
-FOR INSERT 
-WITH CHECK (true);
--- .bolt/supabase_discarded_migrations/20250803114219_d38968fc-ba91-48af-b2c6-f69d99ff90e4.sql
+++
@@ -1,2 +0,0 @@
--- Add clan_master to the user_role enum
-ALTER TYPE user_role ADD VALUE 'clan_master';
